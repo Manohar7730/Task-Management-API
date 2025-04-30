d778fd620b6fdebdfeded14f9a6ab3a15ba6c2de
pediredla manohar <manohar20century@gamil.com>
2025-04-30 12:32:24 +0530

    connected to mongodb local successfully

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
+const connectDB = require("./config/mongoose");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-
-})
-
-app.get('/', (req, res) => {
-    res.send('Task Manager API is running...');
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
   });
-  
-  module.exports = app;
+});
+
+app.get("/", (req, res) => {
+  res.send("Task Manager API is running...");
+});
+
+module.exports = app;
